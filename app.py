af8f5f82b2eff88684f711becbd0b1b1462889c8
Prathamesh Chaudhari <prathameshchaudhari5official@gmail.com>
2022-05-17 12:49:17 +0530

    added secret key

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+# Imports
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'c5272a1ed792b550511e4f43'
+
+
+# Routs for different pages
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
